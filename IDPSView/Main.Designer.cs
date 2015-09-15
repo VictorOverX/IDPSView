b3fe5026a2a5038bb40bcda8b68096a2e1cc0830
Victor Mariano <contato.treinarbh@gmail.com>
2015-09-14 22:16:35 -0300

    Primeiro Commit

---
+++ IDPSView/Main.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace IDPSView
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">verdade se for necessário descartar os recursos gerenciados; caso contrário, falso.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte do Designer - não modifique
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.txtAbrir = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.tbIDPS = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnAbrir);
+            this.groupBox1.Controls.Add(this.txtAbrir);
+            this.groupBox1.Location = new System.Drawing.Point(13, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(424, 54);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "NOR:";
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(332, 17);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(75, 23);
+            this.btnAbrir.TabIndex = 1;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // txtAbrir
+            // 
+            this.txtAbrir.Location = new System.Drawing.Point(69, 19);
+            this.txtAbrir.Name = "txtAbrir";
+            this.txtAbrir.Size = new System.Drawing.Size(257, 20);
+            this.txtAbrir.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSalvar);
+            this.groupBox2.Controls.Add(this.tbIDPS);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(13, 67);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(424, 65);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Dados";
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(332, 24);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 2;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // tbIDPS
+            // 
+            this.tbIDPS.Location = new System.Drawing.Point(69, 26);
+            this.tbIDPS.Name = "tbIDPS";
+            this.tbIDPS.ReadOnly = true;
+            this.tbIDPS.Size = new System.Drawing.Size(257, 20);
+            this.tbIDPS.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IDPS:";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 145);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IDPSView";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.TextBox txtAbrir;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.TextBox tbIDPS;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ IDPSView/Main.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IDPSView
+{
+    public partial class Main : Form
+    {
+
+        string check_s;
+        string IDPS;
+
+        public Main()
+        {
+            InitializeComponent();
+            verNORExist();
+            btnSalvar.Enabled = false;
+        }
+
+        /// <summary>
+        /// ABRIR O ARQUIVO
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            verNORExist(); // Verifica se as Nor existe
+            IDPS = "";
+            System.Windows.Forms.OpenFileDialog openFile = new System.Windows.Forms.OpenFileDialog();
+
+            openFile.Filter         = "BIN Files (.bin)|*.bin|All Files (*.*)|*.*";
+            openFile.FilterIndex    = 1;
+            openFile.Title          = ("Abrir arquivo dump.bin");
+            openFile.FileName       = ("dump.bin");
+
+            txtAbrir.Text           = openFile.FileName;
+
+            if (openFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {                
+                try
+                {
+                    string FileName = openFile.FileName;
+
+                    using (BinaryReader reader = new BinaryReader(new FileStream(FileName, FileMode.Open, FileAccess.Read)))
+                    {
+                        check_s = "";
+
+                        int length = (int)reader.BaseStream.Length;
+                        byte[] myArray = reader.ReadBytes(length);
+
+                        Byte[] metldr;
+
+                        // Verificando se precisa reverter os bytes
+                        reader.BaseStream.Position = 0x200;
+                        metldr = reader.ReadBytes(3);                        
+                        for (int i = 0; i < 3; i++)
+                        {
+                            check_s += Convert.ToChar(metldr[i]);                            
+                        }
+
+                        if (check_s != "IFI")
+                        {
+                            byte chunk_size = 2;
+                            myArray = EndiannessReverse(myArray, chunk_size);
+                        }        
+
+                        // Criando o arquivo
+                        ByteArrayToFile("dump.bin", myArray);
+
+                        // Lendo IDPS
+                        tbIDPS.Text = "";
+
+                        BinaryReader read = new BinaryReader(new FileStream("dump.bin", FileMode.Open));
+
+                        read.BaseStream.Position = 0x2F070;
+                        byte[] idps = read.ReadBytes(16);
+
+                        IDPS = ByteArrayToString(idps).ToUpper();
+                        tbIDPS.Text = IDPS;
+
+
+                        btnSalvar.Enabled = true;
+
+                        read.Close();
+                        reader.Close();
+                    }
+
+                }
+                catch
+                {
+                    MessageBox.Show("Sorry the application seems to have encountered a problem", "Error");
+                } 
+            }
+                      
+        }
+
+        /// <summary>
+        /// VERIFICA SE NOR EXISTE E APAGA
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void verNORExist() 
+        {
+            if (File.Exists("dump.bin"))
+            {
+                File.Delete("dump.bin");
+            }
+        }
+
+        /// <summary>
+        /// VERIFICA SE IDPS EXISTE E APAGA
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void idpsDelete()
+        {
+            if (File.Exists("idps.bin"))
+            {
+                File.Delete("idps.bin");
+            }
+        }
+
+        /// <summary>
+        /// CONVERTER BYTES EM STRING
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public static string ByteArrayToString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+
+
+        /// <summary>
+        /// CONVERTER EM ARQUIVO
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private bool ByteArrayToFile(string _FileName, byte[] _ByteArray)
+        {
+            try
+            {
+                // Open file for reading
+                System.IO.FileStream _FileStream =
+                   new System.IO.FileStream(_FileName, System.IO.FileMode.Create,
+                                            System.IO.FileAccess.Write);
+                // Writes a block of bytes to this stream using data from
+                // a byte array.
+                _FileStream.Write(_ByteArray, 0, _ByteArray.Length);
+
+                // close file stream
+                _FileStream.Close();
+
+                return true;
+            }
+            catch (Exception _Exception)
+            {
+                // Error
+                Console.WriteLine("Exception caught in process: {0}",
+                                  _Exception.ToString());
+            }
+
+            // error occured, return false
+            return false;
+        }
+
+        /// <summary>
+        /// ENDIANNESS REVERSE
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        static byte[] EndiannessReverse(byte[] data, int chunk_size)
+        {
+            byte[] rev = new byte[data.Length];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                int chunk_idx = i / chunk_size;
+                int byte_pos = (chunk_size - 1) - (i % chunk_size);
+                int byte_idx = chunk_idx * chunk_size;
+                rev[i] = data[byte_idx + byte_pos];
+            }
+
+            return rev;
+        }
+
+        /// <summary>
+        /// SALVAR IDPS
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            using (var sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "BIN Files (.bin)|*.bin|All Files (*.*)|*.*";
+                sfd.Title = ("Salvando arquivo IDPS");
+                sfd.RestoreDirectory = true;
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    if (File.Exists("idps.bin"))
+                    {
+                        File.Delete("idps.bin");
+                    }
+
+                    File.WriteAllBytes("idps.bin", StringToByteArray(IDPS));
+                    File.Move("idps.bin", sfd.FileName);   
+                    MessageBox.Show("Arquivo salvo com sucesso!");
+                }
+            }
+        }
+
+        /// <summary>
+        /// CLASSE RESPONSAVEL PELA CRIAÇÃO DO ARQUIVO HEXA
+        /// </summary>
+        /// <param name="hex"></param>
+        /// <returns></returns>
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            verNORExist(); // Verifica se as Nor existe
+            idpsDelete(); // Verifica se IPDS existe e apaga
+        }
+
+
+
+
+    }
+}
